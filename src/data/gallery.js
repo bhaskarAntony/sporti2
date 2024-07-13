8d04987a9019e59c7fcad06434271aec8beac62e
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-07-13 16:20:32 +0530

    all files

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -12,95 +12,95 @@ const gallerydata = [
    {
     id:1,
     title:'Sporti-1 Gallery',
-    image:'./images/events/SportiGallery/1.jpg',
+    image:'./images/events/SportiGallery/1.JPG',
     images:[
         {
-            original:'/images/events/SportiGallery/1.jpg',
-            thumbnail:'/images/events/SportiGallery/1.jpg'
+            original:'/images/events/SportiGallery/1.JPG',
+            thumbnail:'/images/events/SportiGallery/1.JPG'
         },
         {
-            original:'/images/events/SportiGallery/2.jpg',
-            thumbnail:'/images/events/SportiGallery/2.jpg'
+            original:'/images/events/SportiGallery/2.JPG',
+            thumbnail:'/images/events/SportiGallery/2.JPG'
         },
         {
-            original:'/images/events/SportiGallery/3.jpg',
-            thumbnail:'/images/events/SportiGallery/3.jpg'
+            original:'/images/events/SportiGallery/3.JPG',
+            thumbnail:'/images/events/SportiGallery/3.JPG'
         },
         {
-            original:'/images/events/SportiGallery/4.jpg',
-            thumbnail:'/images/events/SportiGallery/4.jpg'
+            original:'/images/events/SportiGallery/4.JPG',
+            thumbnail:'/images/events/SportiGallery/4.JPG'
         },
         {
-            original:'/images/events/SportiGallery/5.jpg',
-            thumbnail:'/images/events/SportiGallery/5.jpg'
+            original:'/images/events/SportiGallery/5.JPG',
+            thumbnail:'/images/events/SportiGallery/5.JPG'
         },
         {
-            original:'/images/events/SportiGallery/6.jpg',
-            thumbnail:'/images/events/SportiGallery/6.jpg'
+            original:'/images/events/SportiGallery/6.JPG',
+            thumbnail:'/images/events/SportiGallery/6.JPG'
         },
         {
-            original:'/images/events/SportiGallery/7.jpg',
-            thumbnail:'/images/events/SportiGallery/7.jpg'
+            original:'/images/events/SportiGallery/7.JPG',
+            thumbnail:'/images/events/SportiGallery/7.JPG'
         },
         {
-            original:'/images/events/SportiGallery/8.jpg',
-            thumbnail:'/images/events/SportiGallery/8.jpg'
+            original:'/images/events/SportiGallery/8.JPG',
+            thumbnail:'/images/events/SportiGallery/8.JPG'
         },
         {
-            original:'/images/events/SportiGallery/9.jpg',
-            thumbnail:'/images/events/SportiGallery/9.jpg'
+            original:'/images/events/SportiGallery/9.JPG',
+            thumbnail:'/images/events/SportiGallery/9.JPG'
         },
         {
-            original:'/images/events/SportiGallery/10.jpg',
-            thumbnail:'/images/events/SportiGallery/10.jpg'
+            original:'/images/events/SportiGallery/10.JPG',
+            thumbnail:'/images/events/SportiGallery/10.JPG'
         },
         {
-            original:'/images/events/SportiGallery/12.jpg',
-            thumbnail:'/images/events/SportiGallery/12.jpg'
+            original:'/images/events/SportiGallery/12.JPG',
+            thumbnail:'/images/events/SportiGallery/12.JPG'
         },
         {
-            original:'/images/events/SportiGallery/13.jpg',
-            thumbnail:'/images/events/SportiGallery/13.jpg'
+            original:'/images/events/SportiGallery/13.JPG',
+            thumbnail:'/images/events/SportiGallery/13.JPG'
         },
         {
-            original:'/images/events/SportiGallery/14.jpg',
-            thumbnail:'/images/events/SportiGallery/14.jpg'
+            original:'/images/events/SportiGallery/14.JPG',
+            thumbnail:'/images/events/SportiGallery/14.JPG'
         },
         {
-            original:'/images/events/SportiGallery/15.jpg',
-            thumbnail:'/images/events/SportiGallery/15.jpg'
+            original:'/images/events/SportiGallery/15.JPG',
+            thumbnail:'/images/events/SportiGallery/15.JPG'
         },
         {
-            original:'/images/events/SportiGallery/16.jpg',
-            thumbnail:'/images/events/SportiGallery/16.jpg'
+            original:'/images/events/SportiGallery/16.JPG',
+            thumbnail:'/images/events/SportiGallery/16.JPG'
         },
         {
-            original:'/images/events/SportiGallery/17.jpg',
-            thumbnail:'/images/events/SportiGallery/17.jpg'
+            original:'/images/events/SportiGallery/17.JPG',
+            thumbnail:'/images/events/SportiGallery/17.JPG'
         },
         {
-            original:'/images/events/SportiGallery/18.jpg',
-            thumbnail:'/images/events/SportiGallery/18.jpg'
+            original:'/images/events/SportiGallery/18.JPG',
+            thumbnail:'/images/events/SportiGallery/18.JPG'
         },
         {
-            original:'/images/events/SportiGallery/19.jpg',
-            thumbnail:'/images/events/SportiGallery/19.jpg'
+            original:'/images/events/SportiGallery/19.JPG',
+            thumbnail:'/images/events/SportiGallery/19.JPG'
         },
         {
-            original:'/images/events/SportiGallery/20.jpg',
-            thumbnail:'/images/events/SportiGallery/20.jpg'
+            original:'/images/events/SportiGallery/20.JPG',
+            thumbnail:'/images/events/SportiGallery/20.JPG'
         },
         {
-            original:'/images/events/SportiGallery/21.jpg',
-            thumbnail:'/images/events/SportiGallery/21.jpg'
+            original:'/images/events/SportiGallery/21.JPG',
+            thumbnail:'/images/events/SportiGallery/21.JPG'
         },
         {
-            original:'/images/events/SportiGallery/22.jpg',
-            thumbnail:'/images/events/SportiGallery/22.jpg'
+            original:'/images/events/SportiGallery/22.JPG',
+            thumbnail:'/images/events/SportiGallery/22.JPG'
         },
         {
-            original:'/images/events/SportiGallery/23.jpg',
-            thumbnail:'/images/events/SportiGallery/23.jpg'
+            original:'/images/events/SportiGallery/23.JPG',
+            thumbnail:'/images/events/SportiGallery/23.JPG'
         }
     ]
    },
@@ -152,55 +152,7 @@ const gallerydata = [
         
     ]
    },
-//    {
-//     id:3,
-//     title:'Mini Theatre Opening Ceremony',
-//     image:'/images/events/MiniTheatreOpening/srwe_2017_1.jpg',
-//     images:[
-//         {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_1.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_1.jpg",
-//           },
-//           {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_2.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_2.jpg",
-//           },
-//           {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_3.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_3.jpg",
-//           },
-//           {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_4.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_4.jpg",
-//           },
-//           {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_5.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_5.jpg",
-//           },
-//           {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_6.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_6.jpg",
-//           },
-//           {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_7.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_7.jpg",
-//           },
-//           {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_8.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_8.jpg",
-//           },
-//           {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_9.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_9.jpg",
-//           },
-//           {
-//             original: "/images/events/MiniTheatreOpening/srwe_2017_10.jpg",
-//             thumbnail: "/images/events/MiniTheatreOpening/srwe_2017_10.jpg",
-//           }
-            
-        
-//     ]
-//    },
+
    {
     id:4,
     title:'Superannuation ceremony of Sri. Pratap Reddy, IPS',
