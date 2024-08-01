8b6baac1fbd3f492e93fcdef370e3757c8eb5b00
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-08-01 19:36:29 +0530

    all files

--- src/components/security/Security.jsx
+++ src/components/security/Security.jsx
@@ -1,13 +1,38 @@
-import { useEffect } from 'react';
+// src/Security.js
+import { useEffect, useState } from 'react';
 
 const Security = () => {
+  const [isIframe, setIsIframe] = useState(false);
+
   useEffect(() => {
     if (window.top !== window.self) {
-      window.top.location = window.self.location;
+      setIsIframe(true);
     }
   }, []);
 
-  return null; // This component doesn't render anything
+  if (isIframe) {
+    return (
+      <div style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'white',
+        textAlign: 'center',
+        zIndex: 9999,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <h1>Restricted</h1>
+        <p>This content is not available in an iframe.</p>
+      </div>
+    );
+  }
+
+  return null; // If not in an iframe, render nothing
 };
 
 export default Security;
